6735eb5d0290d4b32de2e06d607c5d6724dc55cb
ParthAhuja143 <43706009+ParthAhuja143@users.noreply.github.com>
2021-03-16 14:35:38 +0530

    Deployment Changes

--- src/Apolloprovider.js
+++ src/Apolloprovider.js
@@ -7,7 +7,7 @@ import {ApolloProvider} from '@apollo/react-hooks'
 import {setContext} from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-    uri : 'http://localhost:5000'
+    uri : 'https://ancient-retreat-04682.herokuapp.com/'
 })
 
 //To send the authorization in the header (jwtToken)
