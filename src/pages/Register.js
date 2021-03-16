14b902a7558d9688c17b72b5b970ecc73ccb05d1
ParthAhuja143 <43706009+ParthAhuja143@users.noreply.github.com>
2021-03-16 14:43:37 +0530

    Cloudinary Secrets DELETED

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -93,9 +93,9 @@ const Register = (props) => {
         try {
 
             cloudinary.config({
-                cloud_name : 'dx26jwl31' , 
-                api_key : '658131229295366' , 
-                api_secret : 'JL-_y7NGbyIbUfkhoKrVWF2WxEs'
+                cloud_name : process.env.REACT_APP_CLOUDINARY_CLOUD_NAME , 
+                api_key : process.env.REACT_APP_CLOUDINARY_API_KEY , 
+                api_secret : process.env.REACT_APP_CLOUDINARY_API_SECRET
              })
 
              const uploadedResponse = await cloudinary.uploader.upload(base64EncodedFile)
